b59546043c1f6a251ab165db55e2b44068c055c0
Kostya <kostikbushmelev@gmail.com>
2025-10-07 19:58:48 +0300

    Создал проект

---
+++ src/test/java/itmo/soa_backend/SoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.soa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
